69e3d26bd9eb66a959a1b7a8970ee3a9435dd845
남궁혜진 <nkhj0404@gmail.com>
2022-10-04 18:44:05 +0900

    그래프 test

--- front/src/Router/Home.tsx
+++ front/src/Router/Home.tsx
@@ -1,7 +1,55 @@
 import React from "react";
 import { useRecoilState } from "recoil";
 import { AAtom } from "../atom";
+import LineChart from "../components/LineChart";
+import BarChart from "../components/BarChart";
 export default function Home() {
-    const [a, setA] = useRecoilState(AAtom);
-    return <div>Home</div>;
+  const [a, setA] = useRecoilState(AAtom);
+
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const datas = [0, 10, 5, 2, 20, 30, 45];
+
+  const linedata = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "#d50000",
+        borderColor: "#4a148c",
+        data: datas,
+      },
+    ],
+  };
+
+  const bardata = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: ["#ffcdd2", "#fbe9e7", "#e8f5e9"],
+        borderColor: "#4a148c",
+        data: datas,
+      },
+    ],
+  };
+
+  // const options = {
+  //   scaleShowLabelBackdrop: true,
+  //   legend: {
+  //     display: true,
+
+  //     labels: {
+  //       fontColor: "rgb(255, 255, 255)",
+  //       fontSize: 16,
+  //     },
+  //   },
+  // };
+
+  return (
+    <>
+      <div>Home</div>
+      <LineChart chartdata={linedata} />
+      <BarChart chartdata={bardata} />
+    </>
+  );
 }
