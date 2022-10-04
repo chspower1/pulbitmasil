0bf0aec2bde8f9823567ea25fae8f33f270f07ff
남궁혜진 <nkhj0404@gmail.com>
2022-10-04 16:09:54 +0900

    라우터 추가

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Router/Home";
+import About from "./Router/About";
 
 export default function Router() {
-    return <div>Router</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
