b9041b9bc28670f4fd1744c79e360f1554903444
zzeong <zzeong315@gamil.com>
2022-10-05 14:18:14 +0900

    fix: trash_chart

--- front/src/components/BarChart.tsx
+++ front/src/components/BarChart.tsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
 import { Bar } from "react-chartjs-2";
-import { faker } from "@faker-js/faker";
 import json from "../test_data/new_trash_count.json";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 interface TrashCount {
   [key: string]: {
     담배꽁초: number;
-    "일반+담배꽁초": number;
+    일반담배꽁초: number;
     일반쓰레기: number;
     재활용쓰레기: number;
     항아리형: number;
@@ -37,6 +36,11 @@ interface Data {
       data: number[];
       backgroundColor: string;
     },
+    {
+      label: string;
+      data: number[];
+      backgroundColor: string;
+    },
   ];
 }
 
@@ -44,14 +48,28 @@ export default function BarChart() {
   const [trash, setTrash] = useState<TrashCount>(json);
   console.log(trash);
   const options = {
-    responsive: true,
     plugins: {
-      legend: {
-        position: "top" as const,
-      },
       title: {
         display: true,
-        text: "Chart.js Bar Chart",
+        text: '서울시 자치구별 쓰레기통 현황',
+      },
+      legend: {
+        position: 'top' as const,
+      },
+    },
+    responsive: false,
+    scales: {
+      x: {
+        stacked: true,
+        grid: {
+          display: false
+        }
+      },
+      y: {
+        stacked: true,
+        // grid: {
+        //   display: false
+        // }
       },
     },
   };
@@ -73,14 +91,19 @@ export default function BarChart() {
       {
         label: "담배꽁초",
         data: labels.map(i => trash[i].담배꽁초),
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        backgroundColor: "rgba(53, 235, 68, 0.5)",
       },
       {
         label: "항아리형",
         data: labels.map(i => trash[i].항아리형),
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
+        backgroundColor: "rgba(229, 190, 72, 0.5)",
+      },
+      {
+        label: "일반+담배꽁초",
+        data: labels.map(i => trash[i].일반담배꽁초),
+        backgroundColor: "rgba(72, 190, 229, 0.5)",
       },
     ],
   };
-  return <Bar options={options} data={data} />;
+  return <Bar options={options} data={data} width={800} height={500}/>;
 }
