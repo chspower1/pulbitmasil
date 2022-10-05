1861affd65f057739dbaba151bc19f218e446463
남궁혜진 <nkhj0404@gmail.com>
2022-10-05 23:18:47 +0900

    feat: 아이콘 적용

--- front/src/components/ScrollBtn.tsx
+++ front/src/components/ScrollBtn.tsx
@@ -1,15 +1,26 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function ScrollBtn() {
   const [isScroll, setIsScroll] = useState(true);
-
   useEffect(() => {
     window.addEventListener("scroll", handleScroll); // 스크롤 이벤트 등록
     return () => {
       window.removeEventListener("scroll", handleScroll); // 스크롤 이벤트 제거
     };
   }, []);
+
+  const btnVariation = {
+    whileHover: {
+      scale: 1.1,
+    },
+    whileTap: {
+      scale: 0.9,
+    },
+  };
 
   const handleScroll = () => {
     const scrollTop = document.getElementById("app")?.scrollTop;
@@ -30,11 +41,27 @@ export default function ScrollBtn() {
     });
   };
 
-  return <BtnContainer>{isScroll && <button onClick={scrollToTop}></button>}</BtnContainer>;
+  return (
+    <BtnContainer>
+      {isScroll && (
+        <Btn whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} onClick={scrollToTop}>
+          <FontAwesomeIcon icon={faAngleDoubleUp} />
+        </Btn>
+      )}
+    </BtnContainer>
+  );
 }
 
 const BtnContainer = styled.div`
   position: fixed;
   right: 20px;
   bottom: 20px;
+`;
+
+const Btn = styled(motion.button)`
+  width: 50px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
