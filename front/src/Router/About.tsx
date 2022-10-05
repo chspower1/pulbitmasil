1bb280d9ef1c331a88478d0af0b6f7b882ba7698
chspower1 <chspower1@gmail.com>
2022-10-05 20:09:39 +0900

    docs:해야할일

--- front/src/Router/About.tsx
+++ front/src/Router/About.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import styled from "styled-components";
 
 export default function About() {
-    return (
+  return (
     <div>
-        <Btn>Hello</Btn>
-        <Btn>it's button</Btn>
-        <Btn>주변 찾아보기</Btn>
-    </div>);
+      <Btn>Hello</Btn>
+      <Btn>it's button</Btn>
+      <Btn>주변 찾아보기</Btn>
+    </div>
+  );
 }
 
 const Btn = styled.button`
-    margin-top: 200px;
+  margin-top: 200px;
 `;
