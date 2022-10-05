3f61ec5570df01334d4c2183e62aec3bdeb7f533
남궁혜진 <nkhj0404@gmail.com>
2022-10-05 17:31:47 +0900

    error: 회원가입 폼

--- front/src/Router/index.tsx
+++ front/src/Router/index.tsx
@@ -6,6 +6,7 @@ import Nav from "@components/Nav";
 import Home from "./Home";
 import About from "./About";
 import Map from "./Map";
+import Register from "./Register";
 
 export default function Router() {
   return (
@@ -16,6 +17,7 @@ export default function Router() {
         <Route path="/login" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/map" element={<Map />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
