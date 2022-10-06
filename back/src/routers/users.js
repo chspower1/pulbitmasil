71a5174a4527f6a5c862f4b89e616701d6e45448
EH <chi0727139@gmail.com>
2022-10-06 18:02:08 +0900

    feat: back- login

--- back/src/routers/users.js
+++ back/src/routers/users.js
@@ -29,22 +29,20 @@ router.get("/create", async function (req, res) {
   );
 });
 
-// router.get("/insert", function (req, res) {
-//   maria.query(
-//     'INSERT INTO USER(id, name, email, hasedPassword) VALUES("aaa","bbb","aa2@aa.com","00000")',
-//     function (err, rows, fields) {
-//       if (!err) {
-//         res.send(rows);
-//       } else {
-//         console.log("err : " + err);
-//         res.send(err);
-//       }
-//     },
-//   );
-// });
-
 router.get("/select", function (req, res) {
   maria.query("SELECT * FROM USER", function (err, rows, fields) {
+    if (!err) {
+      res.send(rows);
+    } else {
+      console.log("err : " + err);
+      res.send(err);
+    }
+  });
+});
+
+// 편의를 위한 초기화(롤백안됨)
+router.get("/init", function (req, res) {
+  maria.query("TRUNCATE TABLE USER;", function (err, rows, fields) {
     if (!err) {
       res.send(rows);
     } else {
@@ -66,13 +64,40 @@ router.post("/register", async function (req, res, next) {
       [user_id, name, email, hashedPassword],
       function (err, rows, fields) {
         if (!err) {
-          res.send(rows);
+          res.status(200).json({ success: true, message: "user register success" });
         } else {
           console.log("err : " + err);
           res.send(err);
         }
       },
     );
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 로그인
+router.post("/login", async function (req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    maria.query(`SELECT * FROM USER WHERE email = ?`, [email], async function (err, rows, fields) {
+      if (!err) {
+        const correctPasswordHash = rows[0].hashedPassword;
+        const isPasswordCorrect = await bcrypt.compare(password, correctPasswordHash);
+        if (!isPasswordCorrect) {
+          return res.status(400).json({ success: false, message: "password error" });
+        }
+
+        const secretKey = process.env.JWT_SECRET_KEY || "jwt-secret-key";
+        const token = jwt.sign({ id: rows[0].id }, secretKey);
+
+        res.status(200).json({ success: true, message: "user login success", token: token });
+      } else {
+        console.log("err : " + err);
+        res.send(err);
+      }
+    });
   } catch (error) {
     next(error);
   }
