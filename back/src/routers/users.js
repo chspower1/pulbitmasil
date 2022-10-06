a7074fefd26d8cd63844f053aa93459b51081b3c
EH <chi0727139@gmail.com>
2022-10-06 13:50:13 +0900

    feat: register - 1

--- back/src/routers/users.js
+++ back/src/routers/users.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const maria = require("../db/connect/maria");
 
+const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "User" });
@@ -11,10 +15,10 @@ router.get("/", function (req, res, next) {
 router.get("/create", async function (req, res) {
   maria.query(
     `CREATE TABLE USER (
-    id VARCHAR(20) primary key,
-    email VARCHAR(30),
+    id VARCHAR(50) primary key,
     name VARCHAR(10),
-    hasedPassword VARCHAR(10))`,
+    email VARCHAR(30) UNIQUE,
+    hashedPassword VARCHAR(100))`,
     function (err, rows, fields) {
       if (!err) {
         res.send(rows);
@@ -26,8 +30,22 @@ router.get("/create", async function (req, res) {
   );
 });
 
-router.get("/insert", function (req, res) {
-  maria.query('INSERT INTO USER(DEPART_CODE, NAME) VALUES(5001, "ENGLISH")', function (err, rows, fields) {
+// router.get("/insert", function (req, res) {
+//   maria.query(
+//     'INSERT INTO USER(id, name, email, hasedPassword) VALUES("aaa","bbb","aa2@aa.com","00000")',
+//     function (err, rows, fields) {
+//       if (!err) {
+//         res.send(rows);
+//       } else {
+//         console.log("err : " + err);
+//         res.send(err);
+//       }
+//     },
+//   );
+// });
+
+router.get("/select", function (req, res) {
+  maria.query("SELECT * FROM USER", function (err, rows, fields) {
     if (!err) {
       res.send(rows);
     } else {
@@ -35,6 +53,30 @@ router.get("/insert", function (req, res) {
       res.send(err);
     }
   });
+});
+
+// 회원가입
+router.get("/register", async function (req, res, next) {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user_id = uuidv4();
+
+    maria.query(
+      `INSERT INTO USER(id, name, email, hashedPassword) VALUES(?,?,?,?)`,
+      [user_id, name, email, hashedPassword],
+      function (err, rows, fields) {
+        if (!err) {
+          res.send(rows);
+        } else {
+          console.log("err : " + err);
+          res.send(err);
+        }
+      },
+    );
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
