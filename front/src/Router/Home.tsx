f2619e5fbcbb58468bee918e2094c6a51b41d6d4
chspower1 <chspower1@gmail.com>
2022-10-08 21:54:15 +0900

    fixed : 슬라이더 끊김

--- front/src/Router/Home.tsx
+++ front/src/Router/Home.tsx
@@ -4,9 +4,9 @@ import { getInfo } from "@api/api";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { AnimatePresence, motion } from "framer-motion";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 //Variants
 const HomeImgVariants = {
@@ -52,6 +52,11 @@ export default function Home() {
       console.log("Click! and nextState:", next);
     }
   };
+  const onClickImgPoint = (imgIndex: number, num: number) => {
+    toggleLeaving();
+    setImgIndex(num);
+    imgIndex < num ? setNext(true) : setNext(false);
+  };
 
   useEffect(() => {
     const URL = {
@@ -74,30 +79,41 @@ export default function Home() {
           alt="#"
         />
         <ImgPointerBox>
-          {/* [1,2,3,4].map(num=>) */}
-          <ImgPointer />
-          <ImgPointer />
-          <ImgPointer />
-          <ImgPointer />
+          {[1, 2, 3, 4].map(num => (
+            <ImgPointer
+              key={num}
+              className={imgIndex === num ? "active" : "normal"}
+              onClick={() => onClickImgPoint(imgIndex, num)}
+            />
+          ))}
         </ImgPointerBox>
-        </AnimatePresence>
-        <RightBtn onClick={() => onClickArrowBtn(true)}><FontAwesomeIcon icon={faChevronRight} size={'4x'}/></RightBtn>
-        <LeftBtn onClick={() => onClickArrowBtn(false)}><FontAwesomeIcon icon={faChevronLeft} size={'4x'}/></LeftBtn>
-      <div></div>
+      </AnimatePresence>
+      <RightBtn onClick={() => onClickArrowBtn(true)}>
+        <FontAwesomeIcon icon={faChevronRight} size={"4x"} />
+      </RightBtn>
+      <LeftBtn onClick={() => onClickArrowBtn(false)}>
+        <FontAwesomeIcon icon={faChevronLeft} size={"4x"} />
+      </LeftBtn>
     </HomeWrap>
   );
 }
 const HomeWrap = styled.div`
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  overflow-y: hidden;
+  width: 100vw;
+  height: 200vh;
+  overflow: hidden;
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
 const Img = styled(motion.img)`
   position: absolute;
   width: 100vw;
+  /* width: ${window.innerWidth}; */
   height: 100vh;
   object-fit: cover;
+  overflow: hidden;
 `;
 const RightBtn = styled(motion.button)`
   background-color: transparent;
@@ -105,15 +121,15 @@ const RightBtn = styled(motion.button)`
   width: 100px;
   top: 50%;
   right: 0;
-  &:hover{
-            background-color: transparent;
-            transform: scale(1.2);
-        }
+  &:hover {
+    background-color: transparent;
+    transform: scale(1.2);
+  }
 `;
 const LeftBtn = styled(RightBtn)`
   left: 0;
 `;
-const ImgPointerBox = styled.div`
+const ImgPointerBox = styled(motion.div)`
   position: absolute;
   display: flex;
   justify-content: space-between;
@@ -127,11 +143,15 @@ const ImgPointer = styled(motion.div)`
   width: 10px;
   height: 10px;
   border-radius: 5px;
-  background-color: white;
-  opacity: 0.3;
-  .active {
-    opacity: 0.9;
+  cursor: pointer;
+  transition: all 1s ease;
+  &.normal {
+    background-color: rgba(255, 255, 255, 0.3);
+  }
+  &.active {
+    background-color: rgba(255, 255, 255, 1);
+  }
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.7);
   }
 `;
-
-
