26385ed9f1f2a1f17782e47dc2b8f18cb8696d2c
chspower1 <chspower1@gmail.com>
2022-10-10 01:08:22 +0900

    feat : 소개페이지 예시 작성

--- front/src/components/about/Content.tsx
+++ front/src/components/about/Content.tsx
@@ -8,7 +8,7 @@ export default function Content() {
       anchors={anchors}
       navigation
       navigationTooltips={anchors}
-      sectionsColor={["#7fff00", "#00ffff", "#29ab87"]}
+      sectionsColor={["#f7f7f7", "#C7E1D6", "#d8d8d8"]}
       onLeave={(origin, destination, direction) => {
         console.log("onLeave event", { origin, destination, direction });
       }}
@@ -16,9 +16,24 @@ export default function Content() {
         console.log("render prop change", state, fullpageApi);
         return (
           <ContentWrap>
-            <Section01 id="1" className="section"></Section01>
-            <Section02 id="2" className="section"></Section02>
-            <Section03 id="3" className="section"></Section03>
+            <Section01 id="1" className="section">
+              <Box>
+                <Title>첫번째 컨테츠입니다.</Title>
+                <Description>내용입니다.</Description>
+              </Box>
+            </Section01>
+            <Section02 id="2" className="section">
+              <Box>
+                <Title>두번째 컨테츠입니다.</Title>
+                <Description>내용입니다.</Description>
+              </Box>
+            </Section02>
+            <Section03 id="3" className="section">
+              <Box>
+                <Title>세번째 컨테츠입니다.</Title>
+                <Description>내용입니다.</Description>
+              </Box>
+            </Section03>
           </ContentWrap>
         );
       }}
@@ -35,8 +50,22 @@ const ContentWrap = styled.div`
 `;
 const Section01 = styled.div`
   height: 100vh;
-
-  font-size: 400px;
 `;
 const Section02 = styled(Section01)``;
 const Section03 = styled(Section01)``;
+const Box = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  /* padding-top: 70px; */
+  height: 100vh;
+`;
+const Title = styled.h1`
+  font-size: 36px;
+  color: ${props => props.theme.mainColor};
+`;
+const Description = styled.p`
+  font-size: 22px;
+  color: ${props => props.theme.textColor};
+`;
