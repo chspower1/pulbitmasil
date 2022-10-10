a9386ad211bc35321b733183614946392280ee4b
zzeong <zzeong315@gamil.com>
2022-10-10 11:38:33 +0900

    feat: walktable 하는 중

--- front/src/components/WalkTable.tsx
+++ front/src/components/WalkTable.tsx
@@ -1,38 +1,81 @@
 import styled from "styled-components";
 import { faker } from "@faker-js/faker";
+import { useTable } from 'react-table';
+import { useMemo } from "react";
 faker.locale = "ko";
 faker.seed(30);
 
-export default function WalkTable( data: any ) {
-  const columns = ["District", "Name", "Catrgory", "Level", "Distance", "Time"];
+const columns = ["area_gu", "course_name", "Catrgory", "Level", "Distance", "Time"];
 
-  type Columns = {
-    District: string
-    Name: string
-    Catrgory: string
-    Level: number
-    Distance: number
-    Time: number
-  }
+interface Tableprops {
+  columns: string,
+  data: any,
+}
 
+export default function Table({ columns, data } : Tableprops) {
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable({
+    columns, data
+  })
+  // Render the UI for your table
   return (
-    <table>
+    <table {...getTableProps()}>
       <thead>
-        <tr>
-          {columns.map((column) => (
-            <th key={column}>{column}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((test:string, index:number) => (
-          <tr key={index}>
-            <td></td>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
           </tr>
         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
       </tbody>
     </table>
+  )
+}
 
+export default function App() {
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Age',
+        accessor: 'age',
+      },
+      {
+        Header: 'Visits',
+        accessor: 'visits',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      },
+      {
+        Header: 'Profile Progress',
+        accessor: 'progress',
+      },
+    ],
+    []
   )
 
+  // const data = useMemo(() => data(20), []);
+  return (
+      <Table columns={columns} data={data} />
+  );
 }
