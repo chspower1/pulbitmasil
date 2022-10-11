5f034708996952de7152415064e374c2e8fb565d
tw <stw8194@gmail.com>
2022-10-11 11:08:52 +0900

    mogodb connection

--- back/src/db/connect/insert.js
+++ back/src/db/connect/insert.js
@@ -6,23 +6,25 @@ var data = require("../../../../data/dodream/organized_dodream.json");
 
 var x = data.data;
 
-for (i = 0; i < x.length; i++) {
-  maria.query(
-    "INSERT INTO WALK(course_level, course_name, cpi_idx, distance, relate_subway, area_gu, lead_time, detail_course, course_category_nm, x, y,cpi_content) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)",
-    [
-      x[i].course_level,
-      x[i].course_name,
-      x[i].cpi_idx,
-      x[i].distance,
-      x[i].relate_subway,
-      x[i].area_gu,
-      x[i].lead_time,
-      x[i].detail_course,
-      x[i].course_category_nm,
-      x[i].x,
-      x[i].y,
-      x[i].cpi_content,
-    ],
-  );
-  console.log(i);
+for (let i = 0; i < x.length; i++) {
+  // console.log(i);
+  for (let j = 0; j < Object.values(x[i].course_name).length; j++) {
+    // console.log(j);
+    maria.query(
+      "INSERT INTO WALK(course_name,course_level,area_gu,reg_date,content,distance,relate_subway,lead_time, detail_course, traffic_info, CPI) VALUES(?,?,?,?,?,?,?,?,?,?,?)",
+      [
+        Object.values(x[i].course_name)[j][0].course_name,
+        Object.values(x[i].course_name)[j][0].course_level,
+        Object.values(x[i].course_name)[j][0].area_gu,
+        Object.values(x[i].course_name)[j][0].reg_date,
+        Object.values(x[i].course_name)[j][0].content,
+        Object.values(x[i].course_name)[j][0].distance,
+        Object.values(x[i].course_name)[j][0].relate_subway,
+        Object.values(x[i].course_name)[j][0].lead_time,
+        Object.values(x[i].course_name)[j][0].detail_course,
+        Object.values(x[i].course_name)[j][0].traffic_info,
+        Object.values(x[i].course_name)[j][0].CPI,
+      ],
+    );
+  }
 }
