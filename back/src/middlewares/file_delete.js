684a217e961c9c4d322bac33e1de69a3cc88138e
EH <chi0727139@gmail.com>
2022-10-14 02:05:31 +0900

    feat: add  review img delete module

---
+++ back/src/middlewares/file_delete.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+const maria = require("../db/connect/maria");
+require("dotenv").config();
+
+global.hostURL = process.env.Upload;
+
+const fileDelete = reviewId => {
+  maria.query(`SELECT reviewImg FROM REVIEW WHERE reviewId = ?`, [reviewId], async function (err, rows, fields) {
+    if (!err) {
+      const imgSrc = rows[0].reviewImg;
+      const imgName = imgSrc.replace(hostURL, "");
+      const imgPath = path.resolve(__dirname, "../../uploads", imgName);
+      fs.unlinkSync(imgPath);
+    } else {
+      res.send(err);
+    }
+  });
+};
+
+module.exports = { fileDelete };
