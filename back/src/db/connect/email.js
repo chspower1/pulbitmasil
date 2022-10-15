657c25cf42941d604f08e54676ac8df9c56424a9
EH <chi0727139@gmail.com>
2022-10-15 20:02:04 +0900

    fix : email 안에 임시 비밀번호 출력

--- back/src/db/connect/email.js
+++ back/src/db/connect/email.js
@@ -1,9 +1,13 @@
 const nodemailer = require("nodemailer");
 const fs = require("fs");
 const path = require("path");
+const Handlebars = require("handlebars");
 
-const emailForpwdChange = async userEmail => {
-  let transporter = nodemailer.createTransport({
+const emailForTempPassword = async (userEmail, name, tempPassword) => {
+  const template = fs.readFileSync(path.join(__dirname, "./index.handlebars"), { encoding: "utf-8" });
+  const compiledTemplate = Handlebars.compile(template);
+
+  const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
     port: 587,
@@ -19,8 +23,8 @@ const emailForpwdChange = async userEmail => {
     from: `"풀빛마실" <grassdrink09@gmail.com>`,
     to: userEmail,
     subject: "비밀번호 초기화",
-    html: fs.readFileSync(path.join(__dirname, "./index.html"), { encoding: "utf-8" }),
+    html: compiledTemplate({ name: name, tempPassword: tempPassword }),
   });
 };
 
-module.exports = emailForpwdChange;
+module.exports = emailForTempPassword;
