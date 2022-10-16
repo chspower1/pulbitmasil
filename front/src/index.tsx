67c30c7a3b63bc2488a524c1f53587b5f9077dcd
chspower1 <chspower1@gmail.com>
2022-10-16 22:09:48 +0900

    feat(query) : suspense 적용

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
@@ -10,7 +10,9 @@ import { GlobalStyled } from "@style/GlobalStyled";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      suspense: true,
       staleTime: 1000 * 60 * 10,
+      cacheTime: 1000 * 60 * 10,
     },
   },
 });
@@ -19,7 +21,9 @@ root.render(
   // <React.StrictMode>
   <RecoilRoot>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Suspense fallback={<div>로딩중</div>}>
+        <App />
+      </Suspense>
     </QueryClientProvider>
   </RecoilRoot>,
 
