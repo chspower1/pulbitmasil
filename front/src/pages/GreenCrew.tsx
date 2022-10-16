d0890b399d071e0d194e585117dc45766a9b5c57
chspower1 <chspower1@gmail.com>
2022-10-16 17:38:13 +0900

    주석

--- front/src/pages/GreenCrew.tsx
+++ front/src/pages/GreenCrew.tsx
@@ -73,7 +73,7 @@ export default function GreenCrew() {
                   <DetailDescription>{selectedGreenCrew?.level}</DetailDescription>
                 </CourseBox>
               </DescBox>
-              <GreenCrewMap greenCrew={selectedGreenCrew!} />
+              {/* <GreenCrewMap greenCrew={selectedGreenCrew!} /> */}
               {/* <GreenCrewMap /> */}
             </FirstContainer>
             <SecondContainer>
