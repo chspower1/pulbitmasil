e937686c1ba80b2436ad9887d814c7c3480d1663
chspower1 <chspower1@gmail.com>
2022-10-17 12:15:20 +0900

    ..

--- front/src/api/greenCrew.ts
+++ front/src/api/greenCrew.ts
@@ -1,9 +1,14 @@
+import GreenCrew from "@pages/GreenCrew";
+import { IGreenCrew } from "@type/greenCrew";
 import { axiosInstance } from "./axiosInstance";
 
 export async function getGreenCrews() {
   try {
     const { data } = await axiosInstance.get("greencrew");
     console.log(data);
+    // data.map(greenCrew => {
+    //   return { ...greenCrew };
+    // });
     return data;
   } catch (err) {
     console.log(err);
