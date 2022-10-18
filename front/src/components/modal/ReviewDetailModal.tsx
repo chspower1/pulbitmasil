63491a1eb9f4ee54692d10d203d215bab48db1a4
chspower1 <chspower1@gmail.com>
2022-10-19 01:06:51 +0900

    fix : 리뷰 디테일 모달에 X버튼 삭제

--- front/src/components/modal/ReviewDetailModal.tsx
+++ front/src/components/modal/ReviewDetailModal.tsx
@@ -48,21 +48,6 @@ export default function ReviewDetailModal({ review }: { review: IReview }) {
             alt="review image"
             //
           ></ReviewImg>
-          <ModalCloseBtn
-            onClick={() => {
-              navigate("/review");
-            }}
-          >
-            <svg width="14" height="14" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M19 3L11 11L3 19M3 3L19 19"
-                stroke="white"
-                stroke-width="5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </ModalCloseBtn>
         </ImgContainer>
         <ContentsContainer>
           <InfoContainer>
