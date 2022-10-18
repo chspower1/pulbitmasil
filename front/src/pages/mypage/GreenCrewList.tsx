7c85f55a0675da1aaff82d80f11f3d79335341ab
chspower1 <chspower1@gmail.com>
2022-10-18 19:12:32 +0900

    fix : 풀빛마실 모임 진행중/완료 구분

--- front/src/pages/mypage/GreenCrewList.tsx
+++ front/src/pages/mypage/GreenCrewList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Container, Title, Wrapper, SubTitle, Desc, Row } from "@style/Layout";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -7,9 +7,37 @@ import { UserGreenCrew } from "@type/user";
 import { List, Item, ContentBox, ItemTitle, ItemDate, ItemCourse, changeDayForm } from "./Home";
 import dayjs from "dayjs";
 export default function GreenCrewList({ greenCrews }: { greenCrews: UserGreenCrew[] | undefined }) {
+  const [inProgressGreenCrew, setInProgressGreenCrew] = useState<UserGreenCrew[] | undefined>();
+  const [doneGreenCrew, setDoneProgressGreenCrew] = useState<UserGreenCrew[] | undefined>();
+
+  // Util
+  const checkInProgress = (greenCrews: UserGreenCrew[]) => {
+    setInProgressGreenCrew(greenCrews?.filter(greenCrew => greenCrew.inProgress! === 1));
+    setDoneProgressGreenCrew(greenCrews?.filter(greenCrew => greenCrew.inProgress! === 0));
+  };
+
+  useEffect(() => {
+    if (greenCrews) checkInProgress(greenCrews);
+  }, []);
   return (
     <List>
-      {greenCrews?.map(greenCrew => (
+      <Title>진행중</Title>
+      {inProgressGreenCrew?.map(greenCrew => (
+        <Item>
+          <ContentBox>
+            <ItemTitle>
+              {greenCrew?.title}
+              <ItemCourse as="span">({greenCrew?.course})</ItemCourse>
+            </ItemTitle>
+            <Box>
+              <ItemDate>{changeDayForm(greenCrew?.startAt)}</ItemDate>
+              <ItemArea>{greenCrew?.area}</ItemArea>
+            </Box>
+          </ContentBox>
+        </Item>
+      ))}
+      <Title>완료</Title>
+      {doneGreenCrew?.map(greenCrew => (
         <Item>
           <ContentBox>
             <ItemTitle>
